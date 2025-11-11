9632c5d6af604b255e6f476056245a97c35d5dbf
Nanxi-13 <selenananseki@gmail.com>
2025-11-11 14:41:24 -0500

    bug fixed
    fixed a bug and added a readme

---
+++ readme.md
@@ -0,0 +1,8 @@
+Tips on operating the Sphero study buddy:
+    1. activate your virtual environment
+    2. run study.py
+
+Other information:
+1. overall designn: Sphero will act as a study buddy that reminds people to take a short break every 20 minutes. It will pick an activity that could range anywhere from yoga to eye exercise. It has corresponding behaviors and prompts for each activity. 
+2. all functions related to Sphero's activity behaviors can be found in sphero_activities.py
+3. all the code related to the program's overall functionality can be found in study.py
--- study.py
+++ study.py
@@ -8,6 +8,7 @@ from utils import *
 import numpy as np
 from sphero_activities import *
 import random
+import asyncio
 
 
 # tips = {"eyes":"Do you want to relax your eyes? You can look at something 20 feet away for 20 seconds!","cross":"I know a good strech! Cross your legs and touch your toes, then slowly come up, verterbrate by verterbrate!","shake":"Shake your arms and legs and loosen your muscle!","yoga":"You can get on the floor and do some baby pose to strech your back!"}
@@ -16,27 +17,83 @@ import random
 # print(tips[tip[0]])
 activities = [0,1,2,3]
 keys = list(tips.keys())
-
 toy = scanner.find_toy(toy_name="SB-69F1")
+# async def main():
+#     # toy = scanner.find_toy(toy_name="SB-69F1")
+#     async with SpheroEduAPI(toy) as droid:
+
+#         random.shuffle(activities)
+#         activity_order = activities[:]
+#         i = 0
+#         while True:
+#             time.sleep(2)
+#             # await asyncio.sleep(5)
+#             speak("It is time to take a break, let's get up and stretch")
+#             tip = keys[activity_order[i]]
+#             speak(tips[tip])
+
+#             # social group
+#             # time.sleep(2)
+#             await asyncio.sleep(2)
+#             if tip == 'eyes':
+#                 eyes()
+#             elif tip == 'cross':
+#                 cross(droid)
+#             elif tip == "shake":
+#                 shake(droid)
+#             elif tip == 'yoga':
+#                 yoga()
+#             # speak("I will play some background music for you while you stretch!")
+#             # play music
+#             speak("Now let's get back to work!")
+
+#             # # physical group
+#             # time.sleep(2)
+#             # if tip == 'eyes':
+#             #     speak("You can juggle me around and focus your attention on me! That will also be a good exercise for your eyes")
+#             #     # Sphero makes sound when being juggled
+#             # elif tip == 'cross':
+#             #     speak("Let's play chase! I will run around the room now and you'll come and get me!")
+#             #     # Sphero runs around
+#             # elif tip == "shake":
+#             #     speak("Shake me up and down with your left hand! Am I heavy?")
+#             #     # makes sound when shaken
+#             #     speak("Now do the same with your right hand!")
+#             #     # makes sound when shaken
+#             # elif tip == "yoga":
+#             #     speak("As you relax, you can also roll me around with your hands! Feel my weight and the texture of my shell. Am I heavy or light? What's the texture of my shell?")
+
+
+#             if i == 3:
+#                 i = 0
+#             else:
+#                 i += 1
+
+
+#     # spin(360, 0.6)
+# asyncio.run(main())
+
+
 with SpheroEduAPI(toy) as droid:
 
     random.shuffle(activities)
     activity_order = activities[:]
     i = 0
     while True:
-        # time.sleep(1200)
+        time.sleep(1200)
+        # await asyncio.sleep(5)
         speak("It is time to take a break, let's get up and stretch")
         tip = keys[activity_order[i]]
         speak(tips[tip])
 
         # social group
-        time.sleep(2)
+        time.sleep(1)
         if tip == 'eyes':
             eyes()
         elif tip == 'cross':
-            cross()
+            cross(droid)
         elif tip == "shake":
-            shake()
+            shake(droid)
         elif tip == 'yoga':
             yoga()
         # speak("I will play some background music for you while you stretch!")
@@ -64,6 +121,3 @@ with SpheroEduAPI(toy) as droid:
             i = 0
         else:
             i += 1
-
-
-    # spin(360, 0.6)
